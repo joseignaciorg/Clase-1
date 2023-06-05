a1c135b470c2b14a4a8acc8209f77c20a72df12c
joseignaciorg <r.g.joseignacio@gmail.com>
2023-06-05 16:52:04 +0200

    Probando interpolacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+const name = 'Pepito';
+const number = 23;
+const boolean = true;
+const isUndefined = undefined;
+const isNull = null;
+const array = [1, 2, 3, 4, 5];
+const object = { a: 1, b: 3 };
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	return (
+		<div>
+			<p>Name:{name}</p>
+			<p>Number:{number}</p>
+			<p>Boolean:{boolean}</p>
+			<p>Undefined:{isUndefined}</p>
+			<p>Null:{isNull}</p>
+			<p>Array:{array}</p>
+			<p>Object:{object.a}</p>
+		</div>
+	);
 };
 
 export default App;
